552484ff7567b31000b57af06701758dab55eb52
Maral <maralyoosefi@gmail.com>
2023-05-03 19:39:41 +0300

    add ingredients and steps to view recipe page-not completed

--- pages/recipe/[id].tsx
+++ pages/recipe/[id].tsx
@@ -21,7 +21,7 @@ interface Recipe {
   }
 
 export default function RecipeView ({recipe} : {recipe: Recipe}) {
-    console.log(recipe)
+    console.log(recipe.ingredients)
     return(
       <>
       <div className="flex w-full w-100" style={{height: '30rem'}}>
@@ -29,12 +29,37 @@ export default function RecipeView ({recipe} : {recipe: Recipe}) {
           <source src="https://otmhkfujxehpaywcdtxa.supabase.co/storage/v1/object/public/funCookStorage/video-2.mp4?t=2023-05-03T14%3A57%3A55.066Z"/>
         </video>      
       </div>
-      <div className="flex justify-center px-48 py-32">
+      <div className="flex justify-between mx-48 px-20 py-32 rounded-lg shadow-lg border-2">
         <div className="mr-4">
-          <h3 className="text-base text-slate-400">Reviewd By 1000 People</h3>
+          <h1 className="font-bold text-gray-700 text-3xl italic tracking-wide mb-2">{recipe.recipe_name}</h1>
+          <h3 className="text-base text-slate-400 mb-12">Reviewd By 1000 People</h3>
+          <div className="flex mt-10">
+            <h2 className="font-bold text-gray-700 text-2xl italic tracking-wide mr-7">Ingredients:</h2>
+            <div>
+              {recipe.ingredients?.map((ingredient, index) => {
+                return <div key={ingredient} className="mb-8">
+                  <span className="number">{index + 1}</span><span>{ingredient}</span>
+                </div>
+              })}
+            </div>
+          </div>
+          <div className="flex mt-10">
+            <h2 className="font-bold text-gray-700 text-2xl italic tracking-wide mr-24">Steps:</h2>
+            <div className="numbered-list pr-32">
+              {recipe.steps?.map((step, index) => {
+                return <div key={step} className="numbered-list">
+                  <div className="numbered-list-number">
+                    {index + 1}
+                  <div className="numbered-list-line"></div>
+                  </div>
+                  <div className="numbered-list-text">{step}</div>
+                </div>
+              })}
+            </div>
+          </div>
         </div>
-        <div className="bg-white rounded-lg shadow-lg">
-          <Image className="object-cover rounded-t-lg object-center w-full h-auto m-auto"  src={recipe.recipe_image ?? bg} alt="Card Image" width={300} height={1000}/>                 
+        <div className="rounded-lg shadow-lg h-24">
+          <Image className="object-cover rounded-lg object-center w-full h-auto"  src={recipe.recipe_image ?? bg} alt="Card Image" width={500} height={1000}/>                 
         </div>
       </div>
     </>
